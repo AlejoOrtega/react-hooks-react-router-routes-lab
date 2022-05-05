6fb0bddc729d5c9ae618c978afbd54da28ec5d10
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-05 10:41:21 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    {actors.map((dir, index) => {
+      return <div key={index}>
+        <h2>{dir.name}</h2>
+        <ul>
+          {dir.movies.map((name, index) => <li key={index}>{name}</li>)}
+        </ul>
+      </div>
+    })}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <>
+    <h1>Movies page</h1>
+      {movies.map((movie, index) => { 
+        return <div key={index}>
+                  <h1>{movie.title}</h1>
+                  <h2>{movie.time}</h2> 
+                  <ul>
+                    {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+                  </ul>
+                </div>
+      })}
+  </>
+  
 }
 
 export default Movies;
